8218d51b24be0fd98738364fe76c543e9f93af91
Jessica Benson <pokeyjess72@gmail.com>
2020-08-25 21:47:14 -0400

    Final touches

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -4,6 +4,7 @@ from custom_user.models import MyUser
 
 admin.site.register(MyUser, UserAdmin)
 
-UserAdmin.fieldsets += ('Custom fields set', {'fields': ('display_name', 'age')}),
+UserAdmin.fieldsets += ('Custom fields set', {'fields': ('display_name', 'age', 'homepage')}),
 
+# https://stackoverflow.com/questions/50396023/django-custom-user-model-fields-not-appearing-in-django-admin/50396172
 
